cde90ecd1687ee0d871f99ae4d6d8853bcd63cae
dejan.vjestica <dvjestica@gmail.com>
2022-05-05 10:58:07 +0200

    Add sass, add indent eslint rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 export const App = () => {
 	return (
