e664948d445803d657a39c16a538c6785b61315a
dejan.vjestica <dvjestica@gmail.com>
2022-05-05 10:58:07 +0200

    Make input controled, clean up styles, add class dinamicaly, add to eslint

--- src/components/goals/goalInput/GoalInput.tsx
+++ src/components/goals/goalInput/GoalInput.tsx
@@ -7,27 +7,42 @@ type GoalInputProps = {
 	onAddGoal: (enteredText: string) => void
 }
 
-export const GoalInput = (props: any) => {
-	const [enteredValue, setEnteredValue] = useState('')
+export const GoalInput = (props: GoalInputProps) => {
+	const [enteredValue, setEnteredValue] = useState({ value: '' })
+	const [isValid, setisValid] = useState(true)
 
 	const goalInputChangeHandler = (
 		event: React.ChangeEvent<HTMLInputElement>
 	) => {
-		setEnteredValue(event.target.value)
+		if (event.target.value.trim().length > 0) {
+			setisValid(true)
+		}
+		setEnteredValue({ value: event.target.value })
 	}
 
 	const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
-		props.onAddGoal(enteredValue)
+
+		if (enteredValue.value.trim().length === 0) {
+			setisValid(false)
+			return
+		}
+
+		props.onAddGoal(enteredValue.value)
+		setEnteredValue({ value: '' })
 	}
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className="form-control">
+			<div
+				className={`form-control  ${
+					!isValid ? 'invalid' : ''
+				}`}>
 				<label>Course Goal</label>
 				<input
 					type="text"
 					onChange={goalInputChangeHandler}
+					value={enteredValue.value}
 				/>
 			</div>
 			<Button type="submit">Add Goal</Button>
