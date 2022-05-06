575bfafa3119c52b883916ce69646639d4d211e2
dejan.vjestica <dvjestica@gmail.com>
2022-05-06 09:24:22 +0200

    Work with scss.modules

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,27 +1,12 @@
-import styled from 'styled-components'
+import React from 'react'
+import styles from './Button.module.scss'
 
-export const Button = styled.button`
-	background: #8b005d;
-	border: 1px solid #8b005d;
-	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-	color: white;
-	cursor: pointer;
-	font: inherit;
-	padding: 0.5rem 1.5rem;
-	width: 100%;
+type ButtonProps = {
+	type?: 'button' | 'submit' | 'reset'
+	onClick?: () => void
+	children: React.ReactNode
+}
 
-	&:focus {
-		outline: none;
-	}
-
-	&:hover,
-	&:active {
-		background: #ac0e77;
-		border-color: #ac0e77;
-		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-	}
-
-	@media (min-width: 768px) {
-		width: auto;
-	}
-`
+export const Button = (props: ButtonProps) => {
+	return <button {...props} className={styles.button} />
+}
