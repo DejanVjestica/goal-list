38353fe04906c4bec983acc4a8bf03db436f33eb
dejan.vjestica <dvjestica@gmail.com>
2022-05-06 08:14:04 +0200

    Add styled to input

--- src/components/goals/goalInput/GoalInput.tsx
+++ src/components/goals/goalInput/GoalInput.tsx
@@ -1,11 +1,42 @@
 import React, { useState } from 'react'
 
+import styled from 'styled-components'
 import { Button } from '../../UI/Button/Button'
-import './GoalInput.scss'
 
 type GoalInputProps = {
 	onAddGoal: (enteredText: string) => void
 }
+type FormControlProps = {
+	invalid: boolean
+}
+
+const FormControl = styled.div<FormControlProps>`
+	margin: 0.5rem 0;
+
+	label {
+		display: block;
+		font-weight: bold;
+		margin-bottom: 0.5rem;
+		color: ${(props) => (props.invalid ? 'red' : 'black')};
+	}
+
+	input {
+		border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+		display: block;
+		font: inherit;
+		line-height: 1.5rem;
+		padding: 0 0.25rem;
+		width: 100%;
+		color: ${(props) => (props.invalid ? 'red' : 'black')};
+		background-color: ${(props) => props.invalid && 'red'}
+
+		&:focus {
+			background: #fad0ec;
+			border-color: #8b005d;
+			outline: none;
+		}
+	}
+`
 
 export const GoalInput = (props: GoalInputProps) => {
 	const [enteredValue, setEnteredValue] = useState({ value: '' })
@@ -34,17 +65,14 @@ export const GoalInput = (props: GoalInputProps) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div
-				className={`form-control  ${
-					!isValid ? 'invalid' : ''
-				}`}>
+			<FormControl invalid={!isValid}>
 				<label>Course Goal</label>
 				<input
 					type="text"
 					onChange={goalInputChangeHandler}
 					value={enteredValue.value}
 				/>
-			</div>
+			</FormControl>
 			<Button type="submit">Add Goal</Button>
 		</form>
 	)
